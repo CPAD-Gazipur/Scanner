7e04cf7d27475a1f30e8cc3e977d635ac469a31d
zobayer995 <zobayer.hp5@gmail.com>
2022-01-27 14:34:27 +0600

    Scanner initial Commit

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:qrcode/homePage.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: MyApp(),
+    debugShowCheckedModeBanner: false,
+  ));
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return HomePage();
+  }
+}
